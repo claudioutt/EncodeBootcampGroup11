17da96a1441bf871bd8ce93e7841dd80d8c41d06
Wafer102 <59832951+Wafer102@users.noreply.github.com>
2023-05-08 11:47:27 +0200

    Create Voting.ts

---
+++ week2/Voting.ts
@@ -0,0 +1,34 @@
+import { ethers } from 'ethers';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const providerUrl = 'https://goerli.infura.io/v3/845041c982504a089d1f73b93196f756';
+const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+
+const privateKey = process.env.PRIVATE_KEY!;
+const wallet = new ethers.Wallet(privateKey, provider);
+
+const ABI = [{"inputs":[{"internalType":"bytes32[]","name":"proposalNames","type":"bytes32[]"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"chairperson","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"delegate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"voter","type":"address"}],"name":"giveRightToVote","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"proposals","outputs":[{"internalType":"bytes32","name":"name","type":"bytes32"},{"internalType":"uint256","name":"voteCount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"proposal","type":"uint256"}],"name":"vote","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"voters","outputs":[{"internalType":"uint256","name":"weight","type":"uint256"},{"internalType":"bool","name":"voted","type":"bool"},{"internalType":"address","name":"delegate","type":"address"},{"internalType":"uint256","name":"vote","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"winnerName","outputs":[{"internalType":"bytes32","name":"winnerName_","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"winningProposal","outputs":[{"internalType":"uint256","name":"winningProposal_","type":"uint256"}],"stateMutability":"view","type":"function"}]
+
+const contractAddress = '0xcb904C49c150FCdD675AE38326850FB9B15421e3';
+
+const contract = new ethers.Contract(contractAddress, ABI, wallet);
+
+async function interactWithContract() {
+  try { 
+
+    const tx = await contract['vote'](1);
+
+    console.log(`Transaction hash: ${tx.hash}`);
+
+    const receipt = await tx.wait();
+
+    console.log(`Transaction mined: ${receipt.transactionHash}`);
+    console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+interactWithContract();
