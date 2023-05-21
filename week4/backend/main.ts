80c269ceea4442e5f2ea16282867b2c58c9b934e
thalderg <33346659+thalderg@users.noreply.github.com>
2023-05-21 19:22:48 +0200

    Create main.ts

---
+++ week4/backend/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.enableCors(); // to enable to call the swagger from the fron end
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+bootstrap();
